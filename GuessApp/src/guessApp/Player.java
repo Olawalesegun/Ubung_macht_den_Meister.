e4b08901fc6a6bd6223c3aaac10bf3920487918e
Olawalesegun <olawalesegun0@gmail.com>
2023-06-29 20:59:53 +0100

    Dies ist eine Klasse mit dem Titel Guesser, die mit Java als Programmiersprache geschrieben wurde

--- GuessApp/src/guessApp/Player.java
+++ GuessApp/src/guessApp/Player.java
@@ -1,5 +1,38 @@
 package guessApp;
 
-public class Player {
+import java.util.Scanner;
 
+public class Player {
+	private int player1;
+	private int player2;
+	private int player3;
+	Scanner userInput = new Scanner(System.in);
+	
+	
+	public void setPlayer1ToGuessNumber() {
+		print("Hi Player 1, kindly enter your guess");
+		player1 = userInput.nextInt();
+	}
+	public int getPlayer1Guess() {
+		return player1;
+	}
+	
+	public void setPlayer2ToGuessNumber(int player2) {
+		print("Hi Player 2, kindly enter your guess");
+		player2 = userInput.nextInt();
+	}
+	public int getPlayer2Guess() {
+		return player2;
+	}
+	
+	public void setPlayer3ToGuessNumber() {
+		print("Hi Player 3, kindly enter your guess");
+		this.player3 = userInput.nextInt();
+	}
+	public int getPlayer3() {
+		return player3;
+	}
+	public void print(String takeIn) {
+		System.out.println(takeIn);
+	}
 }
