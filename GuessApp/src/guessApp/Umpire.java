5e8c6560f2ce9d7908c2b1bc4eacc6a6ebca88b9
Olawalesegun <olawalesegun0@gmail.com>
2023-06-30 17:26:09 +0100

    Dies ist eine Klasse mit dem Titel Guesser, die mit Java als Programmiersprache geschrieben wurde

--- GuessApp/src/guessApp/Umpire.java
+++ GuessApp/src/guessApp/Umpire.java
@@ -1,5 +1,28 @@
 package guessApp;
 
 public class Umpire {
+	private int numberTobeGuessed;
+	private int player1GuessedNumber;
+	private int player2GuessedNumber;
+	Player player = new Player();
+	Guesser guess = new Guesser();
+	
+	public void setNumberToBeGuessed(){
+		guess.setNumberToBeGuessed();
+		numberTobeGuessed = guess.getNumberToBeGuessed()
+	}
+	public int getNumberToBeGuessed() {
+		return numberTobeGuessed;
+	}
+	
+	
+	public void player1PicksAGuess() {
+		
+		player.setPlayer1ToGuessNumber();
+		player1GuessedNumber = player.getPlayer1Guess();
+	}
+	public void player2PicksAGuess() {
+		player.setPlayer2ToGuessNumber(player2GuessedNumber);
+	}
 
 }
