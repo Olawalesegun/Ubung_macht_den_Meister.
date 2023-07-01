dd8bb4383d4addb6a3500066738be4506ff6ef5b
Olawalesegun <olawalesegun0@gmail.com>
2023-07-01 22:20:30 +0100

    Dies ist eine Klasse mit dem Titel Guesser, die mit Java als Programmiersprache geschrieben wurde

--- GuessApp/src/guessApp/Guesser.java
+++ GuessApp/src/guessApp/Guesser.java
@@ -1,5 +1,15 @@
 package guessApp;
+import java.security.SecureRandom;
 
 public class Guesser {
+	private int numberToBeGuessed;
+	SecureRandom secRan = new SecureRandom();
 
+	public int getNumberToBeGuessed() {
+		return numberToBeGuessed;
+	}
+
+	public void setNumberToBeGuessed() {
+		this.numberToBeGuessed = secRan.nextInt(2, 4);
+	}
 }
