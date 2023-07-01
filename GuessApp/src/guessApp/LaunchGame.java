5650b2df75d8cae514beb161649de788d9629a1b
Olawalesegun <olawalesegun0@gmail.com>
2023-07-01 22:38:28 +0100

    Dies ist eine Klasse mit dem Titel Guesser, die mit Java als Programmiersprache geschrieben wurde

--- GuessApp/src/guessApp/LaunchGame.java
+++ GuessApp/src/guessApp/LaunchGame.java
@@ -1,15 +1,13 @@
 package guessApp;
 
 public class LaunchGame {
-	
-	
 	public static void main(String...args) {
 		Umpire ump = new Umpire();
+		ump.print("Hello Welcome to Game Puzzle!!!");
 		ump.setNumberToBeGuessed();
 		ump.player1PicksAGuess();
 		ump.player2PicksAGuess();
 		ump.player3PicksAGuess();
 		ump.compareTo();
 	}
-
 }
